5e25162d3e01628f5db1083e6c104ce5a7c5bb67
Emiliano <grimaldi385@gmail.com>
2024-11-29 18:08:33 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+require("dotenv").config();
+
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
 //process.loadEnvFile();
 // Deshabilitar cors
 app.use((req, res, next) => {
