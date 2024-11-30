35ba33e88287f4161265e0d9faff05e086d7344b
Emiliano <grimaldi385@gmail.com>
2024-11-30 01:01:35 -0300

    .

--- public/js/abm.js
+++ public/js/abm.js
@@ -1,7 +1,4 @@
-import {
-  cambiarIconoSegunTema,
-  temaLocalStorage,
-} from "./pantalla-productos.js";
+const funciones = require("./pantalla-productos.js");
 
 const btnAgregar = document.getElementById("icon");
 const btnEditar = document.querySelectorAll(".btnEditar");
@@ -9,11 +6,11 @@ const btnEliminar = document.querySelectorAll(".btnEliminar");
 const btnActivar = document.querySelectorAll(".btnActivar");
 
 // llamo a la funcion y se la asigno a window.onload (cuando carga la pagina).
-window.onload = temaLocalStorage;
+window.onload = funciones.temaLocalStorage;
 
 //evento click enviado al icono.
 let iconoToggler = document.getElementById("iconoToggler");
-iconoToggler.addEventListener("click", cambiarIconoSegunTema);
+iconoToggler.addEventListener("click", funciones.cambiarIconoSegunTema);
 
 //menu responsive
 const nav = document.getElementById("nav");
