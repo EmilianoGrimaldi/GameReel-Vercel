63f15fa038192ef8478efc1db3bc01799d693b7c
Emiliano <grimaldi385@gmail.com>
2024-12-04 22:08:56 -0300

    .

--- public/js/abm.js
+++ public/js/abm.js
@@ -3,44 +3,8 @@ const btnEditar = document.querySelectorAll(".btnEditar");
 const btnEliminar = document.querySelectorAll(".btnEliminar");
 const btnActivar = document.querySelectorAll(".btnActivar");
 
-function cambiarIconoSegunTema() {
-  if (html.getAttribute("data-bs-theme") == "light") {
-    iconoToggler.classList.remove("bi-brightness-high-fill");
-    iconoToggler.classList.add("bi-moon-stars-fill");
-    html.setAttribute("data-bs-theme", "dark");
+import { temaLocalStorage, cambiarIconoSegunTema } from "./pantalla-productos";
 
-    body.classList.add("dark-mode");
-
-    localStorage.setItem("theme", "dark");
-  } else {
-    iconoToggler.classList.add("bi-brightness-high-fill");
-    iconoToggler.classList.remove("bi-moon-stars-fill");
-    html.setAttribute("data-bs-theme", "light");
-
-    body.classList.remove("dark-mode");
-
-    localStorage.setItem("theme", "light");
-  }
-}
-
-//funcion que obtiene el tema del local storage guardado antes y setea el tema de la pagina.
-function temaLocalStorage() {
-  let tema = localStorage.getItem("theme");
-  switch (tema) {
-    case "dark":
-      iconoToggler.classList.remove("bi-brightness-high-fill");
-      iconoToggler.classList.add("bi-moon-stars-fill");
-      html.setAttribute("data-bs-theme", "dark");
-      body.classList.add("dark-mode");
-      break;
-    case (null, "light"):
-      iconoToggler.classList.add("bi-brightness-high-fill");
-      iconoToggler.classList.remove("bi-moon-stars-fill");
-      html.setAttribute("data-bs-theme", "light");
-      body.classList.remove("dark-mode");
-      break;
-  }
-}
 // llamo a la funcion y se la asigno a window.onload (cuando carga la pagina).
 window.onload = temaLocalStorage;
 
