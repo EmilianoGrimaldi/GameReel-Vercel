b7c31e68caf96bb14c7b7d6b59e8f14806e7ae7d
Emiliano <grimaldi385@gmail.com>
2024-12-05 13:07:51 -0300

    .

--- routes/abm.routes.js
+++ routes/abm.routes.js
@@ -4,7 +4,7 @@ const Producto = require("../model/producto.js");
 const ProductoSequelize = require("../entity/producto.entity.js");
 const multer = require("multer");
 const { Blob } = require("buffer");
-
+const { put } = require("@vercel/blob");
 /* const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const mimetype = file.mimetype;
@@ -31,7 +31,7 @@ async function subirImagenesNube(blob, filename) {
       throw new Error("Error al obtener la URL del archivo subido");
     }
 
-    return { url: response.url }; 
+    return { url: response.url };
   } catch (error) {
     console.error("Error al subir el archivo a Vercel Blob Storage:", error);
     throw error;
